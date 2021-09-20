37bdd7b2c6f4f12c424f7fd282793bcb31b0b675
Jonas Krödel <jokroedel@gmail.com>
2021-09-20 21:19:31 +0200

    20.09.21

--- src/matrizen/aufgabe3.java
+++ src/matrizen/aufgabe3.java
@@ -1,11 +1,13 @@
 package matrizen;
 
 public class aufgabe3 {
-    public static void main(String[] args) { ausgabe(); }
-
+    public static void main(String[] args) {
+        int[][] mat = new int[10][10];
+        ausgabe(mat);
+    }
     private static void ausgabe(int[][] mat) {
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; i < mat[0].length; j++) {
+        for (int i = 1; i < mat.length; i++) {
+            for (int j = 1; i < mat[0].length; j++) {
                 System.out.print(mat[i][j]);
             }
             System.out.println();
