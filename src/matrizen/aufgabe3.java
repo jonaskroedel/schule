01121562627c11b91c57510c2be616fd9632663f
Jonas Krödel <jokroedel@tsn.at>
2021-09-23 15:27:45 +0200

    23.09.21

--- src/matrizen/aufgabe3.java
+++ src/matrizen/aufgabe3.java
@@ -2,15 +2,14 @@ package matrizen;
 
 public class aufgabe3 {
     public static void main(String[] args) {
-        int[][] mat = new int[5][5];
+        int[][] mat = new int[10][10];
         ausgabe(mat);
     }
 
     private static void ausgabe(int[][] mat) {
         for (int i = 0; i < mat.length; i++) {
-
             for (int j = 0; j < mat[i].length; j++) {
-                mat[i][j] = i*5+j;
+                mat[i][j] = (i*10+j) + 1;
                 System.out.printf("%02d ", mat[i][j]);
             }
             System.out.println();
